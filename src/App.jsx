5f0d46d51ab569702b3dcc4c86a69adea0f0744f
Augusto Mallmann <augusto@MacBook-Pro-de-Augusto.local>
2020-12-11 21:07:39 -0300

    Front end completed

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState} from 'react';
+import {  ThemeProvider, createMuiTheme } from '@material-ui/core'
+import Home from './Home'
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary:{
+        main: '#f44336'
+      },
+      secondary:{
+        main: '#3EA6FF'
+      },
+      background:{
+        default: darkMode ? '#232323' : '#ffffff', 
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#ffffff',
+      }
+
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>  
+    </ThemeProvider>
+  );
+}
+
+export default App;
